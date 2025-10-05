3887edac775d7b249506f40b7e3e63b112482d72
Derek Ensign <derekensign@gmail.com>
2025-10-05 17:45:25 -0500

    Fix function declaration syntax for proper ES6 exports

--- api/webhook/index.js
+++ api/webhook/index.js
@@ -16,7 +16,7 @@ async function query(text, params) {
   }
 }
 
-export default async function handler(req, res) {
+async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
